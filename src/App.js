99986b814b0b157e82cd1a31a03309e2570b1861
dariorojas6 <dario43055@gmail.com>
2023-04-29 09:51:02 -0300

    Comp Store

--- src/App.js
+++ src/App.js
@@ -2,19 +2,23 @@ import { Route, Routes, BrowserRouter } from "react-router-dom"
 import Index from "./api/pages/index"
 import Create from "./api/pages/create.jsx"
 import View from "./api/pages/view.jsx"
+import Store from "./api/store/store"
 
 function App() {
   return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />}></Route>
-          <Route path="/create" element={<Create />}></Route>
-          <Route path="/view/:bookId" element={<View />}></Route>
-          
-        </Routes>
-      </BrowserRouter>
-    </div>
+   
+      <Store>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />}></Route>
+            <Route path="/create" element={<Create />}></Route>
+            <Route path="/view/:bookId" element={<View />}></Route>
+
+          </Routes>
+        </BrowserRouter>
+      </Store>
+   
+
   );
 }
 
---
+++ src/api/store/store.js
@@ -0,0 +1,4 @@
+
+export default function Store({children}){
+return <div>{children}</div>
+}
